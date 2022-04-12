efae874ab57bac1af03350b3136fd085b29f6bfd
Manav Khadka <manavkhadka2004@gmail.com>
2022-04-12 22:14:50 +0530

    commit changes

---
+++ helloword.cpp
@@ -0,0 +1,18 @@
+//user can vote if he is over 18
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"kitne tak number chiye:";
+    cin>>n;
+    for (int i = n; i >= 1; i--)
+    {
+       cout<<i<<" ";
+    }
+        
+    
+
+
+}
