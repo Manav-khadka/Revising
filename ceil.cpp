b2f8f8d1a1b23bf4e3cf317b56d06524d00cf819
Manav Khadka <manavkhadka2004@gmail.com>
2022-04-12 22:23:47 +0530

    msg

---
+++ ceil.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  double num = 10.001;
+
+  double result = ceil(num);
+
+  cout << "Ceil of " << num << " = " << result;
+
+  return 0;
+}
